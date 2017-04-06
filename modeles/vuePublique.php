599ab0c5f1cefec9740ea5f1e41e6fb4c0c13203
sudwebdesign <thomas@sudwebdesign.fr>
2017-04-06 17:34:47 +0200

    clean vuePublique

--- modeles/vuePublique.php
+++ modeles/vuePublique.php
@@ -4,18 +4,13 @@ Si vous réutilisez ce fichier dans votre thème, nous vous conseillons de noter
 version : 
 */
 ?>
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo $this->plxMotor->racine . PLX_PLUGINS;?>plxMyShop/js/jquery.min.js?vuePublique"></script>
 <script type="text/javascript">
 jQuery.noConflict();
 </script>
-
-
 <script type='text/javascript' src='<?php echo $d["plxPlugin"]->plxMotor->racine . PLX_PLUGINS;?>plxMyShop/js/libajax.js'></script>
 <script type='text/javascript' src='<?php echo $d["plxPlugin"]->plxMotor->racine . PLX_PLUGINS;?>plxMyShop/js/panier.js'></script>
-
 <script type='text/javascript'>
-
 var error = false;
 var repertoireAjax = '<?php echo $d["plxPlugin"]->plxMotor->racine . PLX_PLUGINS;?>plxMyShop/ajax/';
 var devise = '<?php echo $d["plxPlugin"]->getParam("devise");?>';
@@ -24,19 +19,8 @@ var shoppingCart = null;
 var L_FOR = '<?php echo $d["plxPlugin"]->getlang('L_FOR'); ?>';
 var L_DEL = '<?php echo $d["plxPlugin"]->getlang('L_DEL'); ?>';
 var L_TOTAL = '<?php echo $d["plxPlugin"]->getlang('L_TOTAL_BASKET'); ?>';
-
-
 </script>
-
-<?php
-
-// e-mail de la commande
-
+<?php // e-mail de la commande
 $_SESSION["plxMyShop"]['msgCommand']="";
-
 $d["plxPlugin"]->validerCommande();
-
-
 $this->vue->affichageVuePublique($d["plxPlugin"]);
-
-
