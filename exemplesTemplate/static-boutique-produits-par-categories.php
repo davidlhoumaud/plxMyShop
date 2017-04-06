613a24b165f87bad7315719f1eff16da78110f20
sudwebdesign <thomas@sudwebdesign.fr>
2017-04-06 18:30:47 +0200

    MAJ : Fichiers theme d'exemples

--- exemplesTemplate/static-boutique-produits-par-categories.php
+++ exemplesTemplate/static-boutique-produits-par-categories.php
@@ -1,70 +1,46 @@
 <?php include(dirname(__FILE__) . '/header.php'); ?>
+ <main class="main grid" role="main">
+  <section class="col sml-12">
+   <article class="article static" role="article" id="static-page-<?php echo $plxShow->staticId(); ?>">
+    <header>
+     <h1>
+      <?php $plxShow->staticTitle(); ?>
+     </h1>
+    </header>
+    <section>
+     <?php $plxShow->staticContent(); ?>
+    </section>
+   </article>
+  </section>
+  <section class="col sml-12">
+<?php
+    $plxMyShop = $plxShow->plxMotor->plxPlugins->aPlugins['plxMyShop'];
+    $plxMyShop->donneesModeles["plxPlugin"] = $plxMyShop;
 
-	<main class="main grid" role="main">
-
-		<section class="col sml-12">
-
-			<article class="article static" role="article" id="static-page-<?php echo $plxShow->staticId(); ?>">
-
-				<header>
-					<h1>
-						<?php $plxShow->staticTitle(); ?>
-					</h1>
-				</header>
-
-				<section>
-					<?php $plxShow->staticContent(); ?>
-				</section>
-
-			</article>
-
-		</section>
-		
-		<section class="col sml-12">
-			
-			<?php
-
-                $plxMyShop = $plxShow->plxMotor->plxPlugins->aPlugins['plxMyShop'];
-                $plxMyShop->donneesModeles["plxPlugin"] = $plxMyShop;
-
-                if (isset($plxMyShop->aProds) && is_array($plxMyShop->aProds)) {
-				echo $plxMyShop->aProds[ $plxMyShop->productNumber()]['name'];
-
-                    foreach ($plxMyShop->aProds as $k1 => $v1) {
-							if ($v1['pcat'] == 1 && $v1['menu'] =='oui') {
-							$cat_array[$k1] = $v1['name']; # can only get the name of a category
-							}
-					}
-
-					foreach($cat_array as $k1 => $v1){
-						echo '<h1 class="title_icon">'.$v1.'</h1>';
-						$plxMyShop->idProduit = $k1;
- 						$plxMyShop->plxShowProductContent();
-						#Now we display a nice name of the categorie....
-						foreach($plxMyShop->aProds as $k => $v) {
-							if (	$v['group'] == $k1
-								&& $v['pcat']  !=1 
-								&&	$v['active']==1 
-								&&	$v['readable']==1
-							) {				
-								$plxMyShop->donneesModeles["k"] = $k;
-								$plxMyShop->modele("espacePublic/boucle/produitRubrique");
-								
-							}
-						}
-					}
-
-
-                }
-
-
-
-
-			?>
-			
-		</section>
-
-	</main>
-
+    if (isset($plxMyShop->aProds) && is_array($plxMyShop->aProds)) {
+     foreach ($plxMyShop->aProds as $k1 => $v1) {
+      if ($v1['pcat'] == 1 && $v1['menu'] =='oui') {
+       $cat_array[$k1] = $v1['name']; # can only get the name of a category
+      }
+     }
+     foreach($cat_array as $k1 => $v1){
+      echo '<h1 class="title_icon">'.$v1.'</h1>';
+      $plxMyShop->idProduit = $k1;
+       $plxMyShop->plxShowProductContent();
+      #Now we display a nice name of the categorie....
+      foreach($plxMyShop->aProds as $k => $v) {
+       if ( strstr($v['group'], $k1)
+        && $v['pcat']  !=1 
+        && $v['active']==1 
+        && $v['readable']==1
+       ) {    
+        $plxMyShop->donneesModeles["k"] = $k;
+        $plxMyShop->modele("espacePublic/boucle/produitRubrique");
+       }
+      }
+     }
+    }
+?>
+  </section>
+ </main>
 <?php include(dirname(__FILE__).'/footer.php'); ?>
-
--- exemplesTemplate/static-boutique-tous-les-produits.php
+++ exemplesTemplate/static-boutique-tous-les-produits.php
@@ -1,36 +1,25 @@
 <?php include(dirname(__FILE__) . '/header.php'); ?>
-
  <main class="main grid" role="main">
-
   <section class="col sml-12">
-
    <article class="article static" role="article" id="static-page-<?php echo $plxShow->staticId(); ?>">
-
     <header>
      <h1>
       <?php $plxShow->staticTitle(); ?>
      </h1>
     </header>
-
     <section>
      <?php $plxShow->staticContent(); ?>
     </section>
-
    </article>
-
   </section>
-  
   <section class="col sml-12">
-   
-   <?php
-    
+<?php
     $plxMyShop = $plxShow->plxMotor->plxPlugins->aPlugins['plxMyShop'];
     $plxMyShop->donneesModeles["plxPlugin"] = $plxMyShop;
-    
+
     if (isset($plxMyShop->aProds) && is_array($plxMyShop->aProds)) {
-     
-     ?>
-      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+?>
+      <script type="text/javascript" src="<?php echo $plxMyShop->plxMotor->racine . PLX_PLUGINS;?>plxMyShop/js/jquery.min.js"></script>
       <script type="text/javascript">
       jQuery.noConflict();
       </script>
@@ -39,7 +28,6 @@
       <script type='text/javascript' src='<?php echo $plxMyShop->plxMotor->racine . PLX_PLUGINS;?>plxMyShop/js/panier.js'></script>
 
       <script type='text/javascript'>
-
       var error = false;
       var repertoireAjax = '<?php echo $plxMyShop->plxMotor->racine . PLX_PLUGINS;?>plxMyShop/ajax/';
       var devise = '<?php echo $plxMyShop->getParam("devise");?>';
@@ -48,46 +36,29 @@
       var L_FOR = '<?php echo $plxMyShop->getlang('L_FOR'); ?>';
       var L_DEL = '<?php echo $plxMyShop->getlang('L_DEL'); ?>';
       var L_TOTAL = '<?php echo $plxMyShop->getlang('L_TOTAL_BASKET'); ?>';
-      
       </script>
-      
       <div id="msgUpDateCart"><?php $plxMyShop->lang('L_PUBLIC_MSG_BASKET_UP'); ?></div>
-
-      <script type="text/JavaScript">
-       var msgUpDateCart = document.getElementById("msgUpDateCart");
-       var shoppingCart = null;
-      </script>
-
-      
-     <?php
-     
+<?php
      foreach ($plxMyShop->aProds as $kRubrique => $vRubrique) {
-      
       if ( $vRubrique['menu'] === 'non'
        || $vRubrique['menu'] === ''
        || (1 !== $vRubrique["active"])
       ) {
        continue;
       }
-      
       $plxMyShop->idProduit = $kRubrique;
       $lien = $plxShow->plxMotor->urlRewrite("index.php?product$kRubrique/{$vRubrique["url"]}");
-      
-      
-      ?>
-       <h2>
-        <a href="<?php echo htmlspecialchars($lien);?>">
-         <?php echo htmlspecialchars($vRubrique['name']);?></a>
-       </h2>
+?>
+       <h2><a href="<?php echo htmlspecialchars($lien);?>"><?php echo htmlspecialchars($vRubrique['name']);?></a></h2>
        <section class="list_products">
         <header>
          <div class="cat_image">
           <?php echo ($vRubrique["image"]!="") ? '<img class="product_image_cat" src="'.$plxMyShop->productImage().'">' : '';?>
          </div>
         </header>
-        <?php
+<?php
          foreach($plxMyShop->aProds as $k => $v) {
-          if ( ($v['group'] === $kRubrique) 
+          if (  strstr($v['group'],$kRubrique) 
            && $v['active']==1 
            && $v['readable']==1
           ) {
@@ -97,20 +68,10 @@
          }
         ?>
        </section>
-      
-      <?php
-      
-      
+<?php
      }
-     
     }
-    
-    
-   ?>
-   
+?>
   </section>
-
  </main>
-
 <?php include(dirname(__FILE__).'/footer.php'); ?>
-
